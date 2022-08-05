c1dad454362c7cbfb00d2d6087ea933b7b28fd91
Sergey Nartov <nse1988@rambler.ru>
2022-08-05 14:30:01 +0300

    validation

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,9 +1,14 @@
 const UserService = require('../service/user-service');
 const ApiError = require('../exception/api-error');
+const { validationResult } = require('express-validator');
 
 class UserController {
   async registration(req, res, next) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return next(ApiError.BadRequest('Ошибка при валидации', errors.array()));
+      }
       const { email, password } = req.body;
       const userData = await UserService.registration(email, password);
       res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
