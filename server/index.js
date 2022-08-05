b8a466ed2a190f0a3ac24c1f5dab5d308c858550
Sergey Nartov <nse1988@rambler.ru>
2022-08-05 11:02:42 +0300

    index

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-app.use('/api', router)
+app.use('/api', router);
 app.use(errorMiddleware);
 
 const start = async () => {
